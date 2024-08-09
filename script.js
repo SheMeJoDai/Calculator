aeae8bc79cdbc0cea9b5e1a52167072e930ad699
緑茶ワーム <117346256+SheMeJoDai@users.noreply.github.com>
2024-08-09 14:18:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function calculate() {
+    let num1 = document.getElementById("num1").value
+    let num2 = document.getElementById("num2").value
+    let result = document.getElementById("result")
+    result.value = Number(num1) + Number(num2)
+}
